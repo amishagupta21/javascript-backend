6d313c0b1333429c06901e8d0799c93865e78f7a
amishagupta21 <amisha@pcds.co.in>
2024-03-29 21:37:46 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 // require('dotenv').config({path:"./env"})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:"./env"
+    path: "./env"
 })
 
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
+    .then(() => {
+        app.on("error", (err) => {
+            console.log("error", err)
+            throw error;
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running on ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("Mongodb_conncetion failed :", err)
+    })
 
 
 
